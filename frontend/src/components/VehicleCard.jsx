06ff7c7c1796255c46d3c84c0b1860098ff7edeb
npratik0 <neupanepratik00@gmail.com>
2025-02-20 16:26:05 +0545

    Currency added

--- frontend/src/components/VehicleCard.jsx
+++ frontend/src/components/VehicleCard.jsx
@@ -129,7 +129,7 @@ const VehicleCard = ({ car }) => {
       <div className="p-4">
         <h3 className="text-lg md:text-xl font-semibold text-gray-800">{car.name}</h3>
         <p className="text-gray-600 text-sm md:text-base">{car.type}</p>
-        <p className="text-blue-600 font-semibold mt-2 text-base md:text-lg">${car.price}/day</p>
+        <p className="text-blue-600 font-semibold mt-2 text-base md:text-lg">NRP{car.price}/day</p>
         <button
           onClick={handleRentNow}
           className="bg-blue-600 text-white w-full py-2 rounded mt-2 hover:bg-blue-700 transition duration-300 cursor-pointer"
