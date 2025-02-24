b5b88f458f7f2a2427d58b7e49256cfe3478d09a
npratik0 <neupanepratik00@gmail.com>
2025-02-24 10:49:46 +0545

    Booking model added

--- backend/src/models/Booking/Booking.js
+++ backend/src/models/Booking/Booking.js
@@ -1,25 +1,56 @@
+// import { DataTypes } from "sequelize";
+// import { sequelize } from "../../database/index.js";
+
+
+// export const Booking = sequelize.define(
+//   "CarBooking",
+//   {
+//     carId: {
+//         type: DataTypes.UUID,
+//         allowNull: false,
+//       },
+//       startDate: {
+//         type: DataTypes.DATE,
+//         allowNull: false,
+//       },
+//       endDate: {
+//         type: DataTypes.DATE,
+//         allowNull: false,
+//       },
+//       totalPrice: {
+//         type: DataTypes.FLOAT,
+//         allowNull: false,
+//       },
+//   },
+// );
+
 import { DataTypes } from "sequelize";
 import { sequelize } from "../../database/index.js";
 
-
-export const Booking = sequelize.define(
-  "Booking",
-  {
-    carId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      startDate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      endDate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      totalPrice: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-      },
+export const Booking = sequelize.define("VehicleBooking", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
   },
-);
+  carId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  userId: { // Include this to establish the relation
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  startDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  endDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  totalPrice: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+});
