0baf9cd13b7486063961be3c7007bc512ea6cd2c
npratik0 <neupanepratik00@gmail.com>
2025-02-25 11:33:45 +0545

    Assosiation of driver bookings, driver and user

--- backend/src/models/associations.js
+++ backend/src/models/associations.js
@@ -1,6 +1,16 @@
 import { Booking } from "./Booking/Booking.js";
 import { User } from "./user/User.js";
+import Driver from "./driver/driver.js";
+import { DriverBooking } from "./DriverBooking.js/DriverBooking.js";
 
 // Define associations
 User.hasMany(Booking, { foreignKey: "userId", as: "Bookings" });
 Booking.belongsTo(User, { foreignKey: "userId", as: "User" });
+
+
+// Define Driver Booking Associations
+User.hasMany(DriverBooking, { foreignKey: "userId", as: "DriverBookings" });
+DriverBooking.belongsTo(User, { foreignKey: "userId", as: "User" });
+
+Driver.hasMany(DriverBooking, { foreignKey: "driverId", as: "DriverBookings" });
+DriverBooking.belongsTo(Driver, { foreignKey: "driverId", as: "Driver" });
