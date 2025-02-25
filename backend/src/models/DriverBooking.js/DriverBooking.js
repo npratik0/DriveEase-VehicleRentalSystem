ccb4941994ef62a084b49f66330989919e252818
npratik0 <neupanepratik00@gmail.com>
2025-02-25 11:31:43 +0545

    Driver Booking model added

---
+++ backend/src/models/DriverBooking.js/DriverBooking.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../../database/index.js";
+
+export const DriverBooking = sequelize.define("DriverBooking", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  driverId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  startDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  endDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  totalPrice: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+});
